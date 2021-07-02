e1d7e2c4293fdc03dfc594760c3859e8cef45ab9
Jan-Willem Mulder <jwnmulder@gmail.com>
2021-07-02 22:32:57 +0200

    added initial tests for resource modules

---
+++ tests/unit/plugins/modules/test_zyxel_vmg8825_facts.py
@@ -0,0 +1,58 @@
+# https://docs.ansible.com/ansible/latest/dev_guide/testing_units_modules.html
+
+from __future__ import absolute_import, division, print_function
+
+from ansible_collections.jwnmulder.zyxel_vmg8825.tests.unit.utils.module_test_utils import (
+    ZyxelModuleTestCase,
+)
+
+__metaclass__ = type
+
+from ansible_collections.ansible.netcommon.tests.unit.modules.utils import (
+    AnsibleFailJson,
+    set_module_args,
+)
+from ansible_collections.jwnmulder.zyxel_vmg8825.plugins.modules import (
+    zyxel_vmg8825_facts,
+)
+
+
+class TestZyxelModuleHttpApi(ZyxelModuleTestCase):
+
+    module = zyxel_vmg8825_facts
+
+    def test_module_fail_when_required_args_missing(self):
+        with self.assertRaises(AnsibleFailJson):
+            set_module_args({})
+            self.module.main()
+
+    def test_ensure_command_called_httpapi(self):
+
+        self.register_connection_call(
+            method="GET",
+            uri="/cgi-bin/DAL?oid=static_dhcp",
+            body={
+                "result": "ZCFG_SUCCESS",
+                "ReplyMsg": "BrWan",
+                "ReplyMsgMultiLang": "",
+                "Object": [
+                    {
+                        "Index": 1,
+                        "BrWan": "Default",
+                        "Enable": True,
+                        "MACAddr": "01:02:03:04:05:06:01",
+                        "IPAddr": "192.168.0.1",
+                    }
+                ],
+            },
+        )
+
+        result = self._run_module(self.module, {})
+
+        self.assertFalse(result["changed"])
+        # self.assertEquals(result["result"], "ZCFG_SUCCESS")
+        # self.assertIsNotNone(result["obj"])
+
+        # args = self.connection.send_request.call_args
+        # self.assertEqual(args[1]["method"].upper(), "GET")
+        # self.assertEqual(args[1]["path"], "/cgi-bin/DAL?oid=static_dhcp")
