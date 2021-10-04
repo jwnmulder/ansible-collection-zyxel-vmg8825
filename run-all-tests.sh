bad2d1fdcc6155aaebf53505b22a7ad4c6789f66
Jan-Willem Mulder <jwnmulder@gmail.com>
2021-10-04 21:58:59 +0200

    ansible-devel workarounds

--- run-all-tests.sh
+++ run-all-tests.sh
@@ -7,4 +7,6 @@ pre-commit run --all-files
 
 ansible-test units -v --color --venv --python 3.8 --debug --requirements
 ansible-test sanity -v --color --docker --python 3.8
-ansible-test network-integration --venv -v
+
+# temporarily until https://github.com/ansible/ansible/issues/75873 is fixed
+ansible-test network-integration --venv -v --color --inventory "$(pwd)/tests/integration/inventory.networking"
