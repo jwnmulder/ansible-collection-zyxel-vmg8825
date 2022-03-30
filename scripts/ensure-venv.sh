01afab1cce8f1a785a579009de5bdec77399ca46
Jan-Willem Mulder <jwnmulder@gmail.com>
2022-03-30 20:58:23 +0200

    venv fixes

--- scripts/ensure-venv.sh
+++ scripts/ensure-venv.sh
@@ -2,27 +2,25 @@
 
 set -euo pipefail
 
-# apt-get install python3.6 python3.6-venv
-SYSTEM_PYTHON=/usr/bin/python3
-
 # First argument should be the venv directory
-VIRTUAL_ENV=$(readlink -f "${1:-.venv}")
+VIRTUAL_ENV_DIR=$(readlink -f "${1:-.venv}")
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 ROOT_DIR=$(readlink -f "${SCRIPT_DIR}/..")
 
-# We might already be in a python virtual venv, using /usr/bin/python3 to make sure we select the right python
-"${SYSTEM_PYTHON}" -m pip install --user --upgrade pip
+# We might already be in a python virtual venv, in that case, skip installing pip
+if [ ! -v VIRTUAL_ENV ]; then
+    python3 -m pip install --user --upgrade pip
+fi
 
-if [ ! -f "${VIRTUAL_ENV}/bin/activate" ]; then
-    "${SYSTEM_PYTHON}" -m venv "${VIRTUAL_ENV}"
+if [ ! -f "${VIRTUAL_ENV_DIR}/bin/activate" ]; then
+    python3 -m venv "${VIRTUAL_ENV_DIR}"
 fi
 
 # shellcheck disable=SC1091
-source "${VIRTUAL_ENV}/bin/activate"
+source "${VIRTUAL_ENV_DIR}/bin/activate"
 
-python3 -m pip install --upgrade pip wheel
+python3 -m pip install --upgrade wheel
 python3 -m pip install --upgrade -r "${ROOT_DIR}/requirements.txt"
 
-set +u
 deactivate
